210134b073983034cef965d63ab392349a993961
DioneCheung <chee-tung-dione_cheung25@milton.edu>
2023-04-13 23:19:57 -0400

    Finished 1-12!

--- src/PatternMaker.java
+++ src/PatternMaker.java
@@ -3,7 +3,7 @@ public class PatternMaker {
         PatternMaker stripes = new PatternMaker();
     }
     public PatternMaker(){
-        pattern6();
+        pattern10();
     }
     public void pattern1(){
         for (int x = 0; x < 6; x++){
@@ -20,6 +20,13 @@ public class PatternMaker {
             System.out.print(i+"\t");
         }
     }
+    public void pattern4(){
+        int num = 1;
+        for (int i = 3; i < 15; i = i+2){
+            System.out.print(num +"\t");
+            num = num + i;
+        }
+    }
     public void pattern5(){
         for (int a = 0; a < 4; a++){
             for (int b = 0; b < 4; b++){
@@ -34,6 +41,103 @@ public class PatternMaker {
                 System.out.print(" ");
             }
             System.out.println(x+ 1);
+        }
+    }
+    public void pattern7(){
+        for (int x = 0; x < 5; x++){
+            for (int y = 5-x ; y > 0;y--){
+                System.out.print(x + 1);
+            }
+            System.out.println();
+        }
+    }
+    public void pattern8(){
+        for (int x = 0; x < 5; x ++){
+            for (int z = 0; z < x; z ++ ){
+                System.out.print(" ");
+            }
+            for (int y = 5-x ; y > 0;y--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public void pattern9(){
+        for (int x = 8; x > -9; x = x-2){
+            int n = Math.abs(x);
+            for (int a = 0; a < (n/2) +1; a ++){
+                System.out.print(" ");
+            }
+            for (int b = 0; b < 9-n; b ++){
+                System.out.print("*");
+            }
+            for (int a = 0; a < (n/2) +1; a ++){
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+    public void pattern10(){
+        //UNFINISHED
+        for (int x = 4; x > -5; x --){
+            int n = 4 - Math.abs(x);
+            for (int y = 0; y < n; y ++){
+                System.out.print(" ");
+            }
+            if (x != 0){
+                System.out.print("*");
+                for (int m = 0; m < 7 - 2*n; m ++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            } else if (x == 0){
+                System.out.print("*");
+            }
+            for (int y = 0; y < n; y ++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+//        for (int x = 9; x > -10; x = x-2) {
+//            int n = Math.abs(x);
+//            for (int y = 0; y > (9 - n)/2; y++){
+//                System.out.print(" ");
+//            }
+//            System.out.print("*");
+//            for (int z = 0; z > n - 2; z++){
+//                System.out.print(" ");
+//            }
+//            System.out.print("*");
+//            for (int y = 0; y > (9 - n)/2; y++){
+//                System.out.print(" ");
+//            }
+//            System.out.println();
+//        }
+    }
+    public void pattern11(){
+        for (int i = 0; i < 5; i ++ ){
+            for (int n = 0; n < i+1; n ++ ){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    public void pattern12(){
+        for (int i = 0; i < 5; i ++ ){
+            for (int n = 0; n < i+1; n ++ ){
+                System.out.print("#");
+            }
+            for (int m = 0; m < 4 - i; m ++){
+                System.out.print(" ");
+            }
+            for (int m = 0; m < 4 - i; m ++){
+                System.out.print(" ");
+            }
+            for (int n = 0; n < i+1; n ++ ){
+                System.out.print("#");
+            }
+            System.out.println("");
         }
     }
 }
